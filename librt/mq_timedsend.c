eb504351487cd4781bc16e38fc063472d53e2d16
Mikael Pettersson <mikpelinux@gmail.com>
2021-08-28 16:20:55 +0200

    v850: prevent referencing non-existent system calls

--- librt/mq_timedsend.c
+++ librt/mq_timedsend.c
@@ -8,9 +8,13 @@
 #include <unistd.h>
 #include <cancel.h>
 
+#ifdef __NR_mq_timedsend
+
 #define __NR___mq_timedsend_nocancel __NR_mq_timedsend
 _syscall5(int, __NC(mq_timedsend), mqd_t, mqdes, const char *, msg_ptr, size_t, msg_len, unsigned int, msq_prio, const struct timespec *, abs_timeout)
 
 CANCELLABLE_SYSCALL(int, mq_timedsend, (mqd_t mqdes, const char *msg_ptr, size_t msq_len, unsigned int msq_prio, const struct timespec *abs_timeout),
 		    (mqdes, msg_ptr, msq_len, msq_prio, abs_timeout))
 lt_libc_hidden(mq_timedsend)
+
+#endif
