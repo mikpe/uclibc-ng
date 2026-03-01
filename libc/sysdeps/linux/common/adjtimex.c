02271999649e3de56447e8a3426ea0e608e8cb49
Mikael Pettersson <mikpelinux@gmail.com>
2026-03-01 21:06:15 +0100

    adjtimex.c: restore adjtimex() syscall for when clock_adjtime() isn't available

--- libc/sysdeps/linux/common/adjtimex.c
+++ libc/sysdeps/linux/common/adjtimex.c
@@ -6,13 +6,20 @@
  * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
  */
 
+#include <sys/syscall.h>
 #include <sys/timex.h>
+
+/* We can only redirect to clock_adjtime() of we have __NR_clock_adjtime or its 64-bit version. */
+#if defined(__NR_adjtimex) && !(defined(__NR_clock_adjtime64) || defined(__NR_clock_adjtime))
+_syscall1(int, adjtimex, struct timex *, buf)
+#else
 #include <time.h>
 
 int adjtimex(struct timex *buf)
 {
     return clock_adjtime(CLOCK_REALTIME, buf);
 }
+#endif
 
 libc_hidden_def(adjtimex)
 weak_alias(adjtimex,__adjtimex)
